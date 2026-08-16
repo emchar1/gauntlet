3e389013d66675e4048afa94f217c25de82df099
Eddie Char <emchar1@gmail.com>
2026-08-16 08:40:44 -0700

    Add movement to Enemy - needed to change path_desired_distance from 1.0 to 2.0 to make enemy move due to their origin being at 1.76, which is > 1 but < 2.

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -8,8 +8,14 @@ enum State {
 }
 
 @onready var animation_player = $AnimationPlayer
+@onready var nav_agent = $NavigationAgent3D
+
+@export var speed := 8.0
 
 var current_state: State
+
+# FIXME: - Remove @export after initializing enemies from a Packed Scene.
+@export var player: Player
 
 
 # FUNCTIONS
@@ -20,6 +26,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	_apply_gravity(delta)
+	_process_movement()
 	
 	move_and_slide()
 
@@ -28,6 +35,28 @@ func _physics_process(delta: float) -> void:
 func _apply_gravity(delta: float):
 	if not is_on_floor():
 		velocity.y += get_gravity().y * delta
+
+
+# Movement and follow player
+func _process_movement():
+	if player == null:
+		print("No player assigned. Assign player to enemy.")
+		return
+	
+	nav_agent.target_position = player.global_position
+	
+	var next_nav_point = nav_agent.get_next_path_position()
+	var path_dir = next_nav_point - global_position
+	
+	path_dir.y = 0
+	path_dir = path_dir.normalized()
+	
+	velocity.x = path_dir.x * speed
+	velocity.z = path_dir.z * speed
+	
+	# Rotate enemy to face player
+	if path_dir.length_squared() > 0.0:
+		look_at(global_position + path_dir, Vector3.UP)
 
 
 # Updates the current state and animation
@@ -48,7 +77,6 @@ func _update_state(state: State):
 func _on_hurtbox_area_entered(area: Area3D) -> void:
 	if area.is_in_group("projectile"):
 		print("Ow")
-	pass # Replace with function body.
 
 
 func damage(amount: float):
