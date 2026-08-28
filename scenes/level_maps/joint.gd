5d807016947e1d241b11f3369bd790457d099a6b
Eddie Char <emchar1@gmail.com>
2026-08-28 16:10:25 -0700

    Added joint script with customizable sides!

---
+++ scenes/level_maps/joint.gd
@@ -0,0 +1,90 @@
+@tool
+extends Node3D
+
+
+# PROPERTIES
+
+enum SideType {
+	WALL, DOOR, NONE
+}
+
+@export var wall_top = SideType.NONE:
+	set(value):
+		wall_top = value
+		_update_side(wall_top, Vector3(0, 0, -half), Vector3.ZERO)
+
+@export var wall_left = SideType.NONE:
+	set(value):
+		wall_left = value
+		_update_side(wall_left, Vector3(-half, 0, 0), Vector3(0, PI / 2, 0))
+
+@export var wall_bottom = SideType.NONE:
+	set(value):
+		wall_bottom = value
+		_update_side(wall_bottom, Vector3(0, 0, half), Vector3.ZERO)
+
+@export var wall_right = SideType.NONE:
+	set(value):
+		wall_right = value
+		_update_side(wall_right, Vector3(half, 0, 0), Vector3(0, PI / 2, 0))
+
+@export var wall_scene: PackedScene
+@export var door_scene: PackedScene
+
+@onready var walls = $Walls
+
+var half: float = 14.5
+
+
+# FUNCTIONS
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_update_all_sides()
+
+
+func _update_all_sides():
+	for child in walls.get_children():
+		child.queue_free()
+	
+	_create_side(wall_top, Vector3(0, 0, -half), Vector3.ZERO)
+	_create_side(wall_left, Vector3(-half, 0, 0), Vector3(0, PI / 2, 0))
+	_create_side(wall_bottom, Vector3(0, 0, half), Vector3.ZERO)
+	_create_side(wall_right, Vector3(half, 0, 0), Vector3(0, PI / 2, 0))
+
+
+func _update_side(side_type: SideType, pos: Vector3, rot: Vector3):
+	if not is_instance_valid(walls):
+		return
+	
+	# Remove existing side at this position.
+	for child in walls.get_children():
+		if child.position == pos:
+			child.queue_free()
+	
+	# Nothing to create.
+	if side_type == SideType.NONE:
+		return
+	
+	_create_side(side_type, pos, rot)
+
+
+func _create_side(side_type: SideType, pos: Vector3, rot: Vector3):
+	var scene: PackedScene
+	
+	match side_type:
+		SideType.WALL:
+			scene = wall_scene
+		SideType.DOOR:
+			scene = door_scene
+		SideType.NONE:
+			return
+	
+	var side = scene.instantiate()
+	side.position = pos
+	side.rotation = rot
+	
+	if side_type == SideType.WALL:
+		side.size.x = 30.0
+	
+	walls.add_child(side)
