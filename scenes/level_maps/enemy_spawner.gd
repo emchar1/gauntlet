58ad29af742b7432e12e63a8ca032b8eb082eff9
Eddie Char <emchar1@gmail.com>
2026-08-29 03:52:37 -0700

    Add corridor_spawn scene, which shares enemy_spawner script. A corridor spawn has a spawn area and a trip wire (collisionshape3d). Move the trip wire to start the spawner.

---
+++ scenes/level_maps/enemy_spawner.gd
@@ -0,0 +1,55 @@
+extends Node3D
+
+# PROPERTIES
+
+@export var total_enemies: int = 9
+@export var enemy_type1: EnemyConfig
+@export var enemy_type2: EnemyConfig
+
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var enemy_timer = $EnemyTimer
+@onready var enemy_spawn_location = $Path3D/PathFollow3D
+
+var current_enemy: int = 0
+
+
+# FUNCTIONS
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _get_enemy_configs() -> Array[EnemyConfig]:
+	return [
+		enemy_type1,
+		enemy_type2
+	]
+
+
+# CALLBACK FUNCTIONS
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		enemy_timer.start()
+
+
+func _on_enemy_timer_timeout() -> void:
+	if current_enemy >= total_enemies:
+		enemy_timer.stop()
+		return
+	
+	current_enemy += 1
+	
+	enemy_spawn_location.progress_ratio = randf()
+	
+	var enemy_config = _get_enemy_configs().pick_random()
+	var enemy = enemy_config.enemy_scene.instantiate()
+	enemy.player = player
+	enemy.enemy_config = enemy_config
+	enemy.spawn()
+	
+	add_child(enemy)
+	
+	# MUST come AFTER add_child!!
+	enemy.global_position = enemy_spawn_location.global_position
